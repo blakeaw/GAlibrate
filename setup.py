7089cae2433a8ab8a2192e34426b8f8650fe0bf1
Blake <blakeaw1102@gmail.com>
2019-07-19 14:52:21 -0500

    Added a distutils setup script for installation.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='galibrate',
+      version='0.1.0',
+      description='Python toolkit for continuous Genetic Algorithm optimization.',
+      author='Blake A. Wilson',
+      author_email='blakeaw1102@gmail.com',
+      url='https://github.com/blakeaw/GAlibrate',
+      packages=['galibrate'],
+      license='MIT',
+      keywords=['continuous genetic algorithm', 'model calibration', 'parameter estimation']
+     )
