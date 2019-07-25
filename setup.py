cf4d88f18cc394f2ac2d6e35d44e839c74e735d6
Blake <blakeaw1102@gmail.com>
2019-07-25 00:06:22 -0500

    Updated the setup.py to change version number and add galibrate.pysb_utils to the packages list.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='galibrate',
-      version='0.2.0',
+      version='0.4.0',
       description='Python toolkit for continuous Genetic Algorithm optimization.',
       author='Blake A. Wilson',
       author_email='blakeaw1102@gmail.com',
       url='https://github.com/blakeaw/GAlibrate',
-      packages=['galibrate'],
+      packages=['galibrate', 'galibrate.pysb_utils'],
       license='MIT',
       keywords=['continuous genetic algorithm', 'model calibration', 'parameter estimation']
      )
