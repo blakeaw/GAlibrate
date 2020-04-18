17d79a6613af60da0fb15fd8d321cb258b9360d7
Blake <blakeaw1102@gmail.com>
2020-04-18 11:07:23 -0500

    Module defining the parallel fitness evaluation over chromosomes.

---
+++ galibrate/par_fitness_eval.py
@@ -0,0 +1,12 @@
+import multiprocessing as mp
+import numpy as np
+
+def par_fitness_eval(fitness_func, chromosomes, first_chromosome, nprocs):
+    eval_chromosomes = chromosomes[first_chromosome:]
+    max_procs = mp.cpu_count()
+    if nprocs > max_procs:
+        nprocs = max_procs
+    pool = mp.Pool(nprocs)
+    fitnesses = pool.map(fitness_func, [chromosome for chromosome in eval_chromosomes])
+    pool.close()
+    return np.array(fitnesses)
