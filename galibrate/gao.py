6c03f7660547a2a73e2f390a9cdca1e2012bd3f5
blakeaw <blakeaw1102@gmail.com>
2023-04-26 17:12:05 -0500

    Applied Black formatting to gao.py

--- galibrate/gao.py
+++ galibrate/gao.py
@@ -1,12 +1,14 @@
 import warnings
 import numpy as np
-#import pandas as pd
+
+# import pandas as pd
 from .par_fitness_eval import par_fitness_eval
 
 _run_gao_import = False
 # Try the numba version of run_gao
 try:
     from . import run_gao_numba as run_gao
+
     _run_gao_import = True
     warnings.warn("------Running GAO with numba optimization.------", RuntimeWarning)
 except ImportError:
@@ -14,16 +16,23 @@ except ImportError:
 # Numba didn't work, so try the Cython version
 if not _run_gao_import:
     try:
-        import pyximport; pyximport.install(language_level=3)
+        import pyximport
+
+        pyximport.install(language_level=3)
         from . import run_gao_cython as run_gao
+
         _run_gao_import = True
-        warnings.warn("------Running GAO with Cython optimization.------", RuntimeWarning)
+        warnings.warn(
+            "------Running GAO with Cython optimization.------", RuntimeWarning
+        )
     except ImportError:
         _run_gao_import = False
-#Numba nor Cython worked, so fallback to the pure Python version
+# Numba nor Cython worked, so fallback to the pure Python version
 if not _run_gao_import:
     from . import run_gao_py as run_gao
+
     _run_gao_import = True
+
 
 class GAO(object):
     """A continuous Genetic Algorithm-based Optimizer.
@@ -48,22 +57,29 @@ class GAO(object):
             https://www.whitman.edu/Documents/Academics/Mathematics/2014/carrjk.pdf
     """
 
-    def __init__(self, sampled_parameters, fitness_function, population_size,
-                 generations=100, mutation_rate=0.1):
-        """Initialize the GAO object.
-        """
+    def __init__(
+        self,
+        sampled_parameters,
+        fitness_function,
+        population_size,
+        generations=100,
+        mutation_rate=0.1,
+    ):
+        """Initialize the GAO object."""
         self.sampled_parameters = sampled_parameters
         self.fitness_function = fitness_function
         if population_size < 4:
             population_size = 4
-        self.population_size = int(population_size/4.) * 4
+        self.population_size = int(population_size / 4.0) * 4
         if self.population_size != population_size:
-            warn_string = "--Population size was adjusted from {} to {} to make it a multiple of four--".format(population_size, self.population_size)
+            warn_string = "--Population size was adjusted from {} to {} to make it a multiple of four--".format(
+                population_size, self.population_size
+            )
             warnings.warn(warn_string)
         self.generations = generations
         self.mutation_rate = mutation_rate
-        #self.survival_rate = survival_rate
-        #self._n_survive = int(survival_rate*population_size)
+        # self.survival_rate = survival_rate
+        # self._n_survive = int(survival_rate*population_size)
         self._n_sp = len(sampled_parameters)
         self._pop_idxs = np.array(list(range(self.population_size)))
         self._last_generation = None
@@ -83,17 +99,34 @@ class GAO(object):
             during the search and the corresponding fitness value:
             (theta, fitness)
         """
-        sp_locs = np.array([sampled_parameter.loc for sampled_parameter in self.sampled_parameters])
-        sp_widths = np.array([sampled_parameter.width for sampled_parameter in self.sampled_parameters])
-        last_gen_chromosomes, best_fitness_per_generation = run_gao.run_gao(self.population_size, self._n_sp,
-                                               sp_locs, sp_widths,
-                                               self.generations, self.mutation_rate,
-                                               self.fitness_function, nprocs)
+        sp_locs = np.array(
+            [sampled_parameter.loc for sampled_parameter in self.sampled_parameters]
+        )
+        sp_widths = np.array(
+            [sampled_parameter.width for sampled_parameter in self.sampled_parameters]
+        )
+        last_gen_chromosomes, best_fitness_per_generation = run_gao.run_gao(
+            self.population_size,
+            self._n_sp,
+            sp_locs,
+            sp_widths,
+            self.generations,
+            self.mutation_rate,
+            self.fitness_function,
+            nprocs,
+        )
 
         if nprocs > 1:
-            fitnesses = par_fitness_eval(self.fitness_function, last_gen_chromosomes, 0, nprocs)
+            fitnesses = par_fitness_eval(
+                self.fitness_function, last_gen_chromosomes, 0, nprocs
+            )
         else:
-            fitnesses = np.array([self.fitness_function(chromosome) for chromosome in last_gen_chromosomes])
+            fitnesses = np.array(
+                [
+                    self.fitness_function(chromosome)
+                    for chromosome in last_gen_chromosomes
+                ]
+            )
         self._last_generation = last_gen_chromosomes
         self._last_generation_fitnesses = fitnesses
         fittest_idx = np.argmax(fitnesses)
@@ -114,18 +147,36 @@ class GAO(object):
             during the search and the corresponding fitness value:
             (theta, fitness)
         """
-        sp_locs = np.array([sampled_parameter.loc for sampled_parameter in self.sampled_parameters])
-        sp_widths = np.array([sampled_parameter.width for sampled_parameter in self.sampled_parameters])
-        last_gen_chromosomes, best_fitness_per_generation = run_gao.continue_gao(self.population_size, self._n_sp,
-                                               self._last_generation, self._last_generation_fitnesses, sp_locs,
-                                               sp_widths,
-                                               generations, self.mutation_rate,
-                                               self.fitness_function, nprocs)
+        sp_locs = np.array(
+            [sampled_parameter.loc for sampled_parameter in self.sampled_parameters]
+        )
+        sp_widths = np.array(
+            [sampled_parameter.width for sampled_parameter in self.sampled_parameters]
+        )
+        last_gen_chromosomes, best_fitness_per_generation = run_gao.continue_gao(
+            self.population_size,
+            self._n_sp,
+            self._last_generation,
+            self._last_generation_fitnesses,
+            sp_locs,
+            sp_widths,
+            generations,
+            self.mutation_rate,
+            self.fitness_function,
+            nprocs,
+        )
 
         if nprocs > 1:
-            fitnesses = par_fitness_eval(self.fitness_function, last_gen_chromosomes, 0, nprocs)
+            fitnesses = par_fitness_eval(
+                self.fitness_function, last_gen_chromosomes, 0, nprocs
+            )
         else:
-            fitnesses = np.array([self.fitness_function(chromosome) for chromosome in last_gen_chromosomes])
+            fitnesses = np.array(
+                [
+                    self.fitness_function(chromosome)
+                    for chromosome in last_gen_chromosomes
+                ]
+            )
         self._last_generation = last_gen_chromosomes
         self._last_generation_fitnesses = fitnesses
         fittest_idx = np.argmax(fitnesses)
@@ -134,8 +185,9 @@ class GAO(object):
         self._fittest_chromosome = fittest_chromosome
         self._fittest_fitness = fittest_fitness
         best_fitness_per_generation[-1] = fittest_fitness
-        self._best_fitness_per_generation = np.concatenate((self._best_fitness_per_generation,
-                                                           best_fitness_per_generation))
+        self._best_fitness_per_generation = np.concatenate(
+            (self._best_fitness_per_generation, best_fitness_per_generation)
+        )
         self._total_generations += generations
         return fittest_chromosome, fittest_fitness
 
@@ -157,4 +209,4 @@ class GAO(object):
 
     @property
     def final_population_fitness(self):
-        return self._last_generation_fitnesses        
+        return self._last_generation_fitnesses
