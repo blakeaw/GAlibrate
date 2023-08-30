b8bd92e7de44ef4f2f1a8ddd73e8a92a8eb4e3f0
blakeaw <blakeaw1102@gmail.com>
2023-08-30 18:44:23 -0500

    Tests for the sampled_parameter module and its SampleParameter class.

---
+++ tests/test_sampled_parameter.py
@@ -0,0 +1,31 @@
+import numpy as np
+from galibrate.sampled_parameter import SampledParameter
+
+def test_init():
+    assert (SampledParameter('test', 0., 1.) is not None)
+
+def test_attributes():
+    sp = SampledParameter('test', 0., 1.)
+    assert sp.name == 'test'
+    assert np.isclose(sp.loc, 0.)
+    assert np.isclose(sp.width, 1.)
+
+def test_member_random():
+    sp = SampledParameter('test', 0., 1.)
+    rand = sp.random(1)
+    assert len(rand) == 1
+    assert rand[0] < (sp.loc + sp.width)
+    assert rand[0] > (sp.loc)    
+
+def test_member_unit_transform():
+    sp = SampledParameter('test', 0., 1.)
+    utrans = sp.unit_transform(0.)
+    assert np.isclose(utrans, sp.loc)
+    utrans = sp.unit_transform(1.)
+    assert np.isclose(utrans, (sp.loc + sp.width))
+
+if __name__ == '__main__':
+    test_init()
+    test_attributes()
+    test_member_random()
+    test_member_unit_transform()
