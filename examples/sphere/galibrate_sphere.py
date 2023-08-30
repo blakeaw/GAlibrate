4b45675b41e8232aebd930ae7f66cf857f788125
blakeaw <blakeaw1102@gmail.com>
2023-08-30 14:36:55 -0500

    Adjusted the population size in the sphere example.

--- examples/sphere/galibrate_sphere.py
+++ examples/sphere/galibrate_sphere.py
@@ -16,6 +16,9 @@ from galibrate.sampled_parameter import SampledParameter
 from galibrate import GAO
 from galibrate.benchmarks import sphere
 
+from galibrate import run_gao_py
+from galibrate import gao
+gao.run_gao = run_gao_py
 
 # Define the fitness function to minimize the 'sphere' objective function.
 # minimum is x=0 and f(x) = 0
@@ -31,11 +34,11 @@ if __name__ == "__main__":
     ]
 
     # Set the active point population size
-    population_size = 100
+    population_size = 1000
 
     # Construct the Genetic Algorithm-based Optimizer.
     gao = GAO(
-        sampled_parameters, fitness, population_size, generations=50, mutation_rate=0.1
+        sampled_parameters, fitness, population_size, generations=100, mutation_rate=0.1
     )
     # run it
     best_theta, best_theta_fitness = gao.run()
